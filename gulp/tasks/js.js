40a3958c1c7d5d6f1ea000817c3ad499e6d096fa
mitch-rickman <mitchrickman@gmail.com>
2016-05-17 21:54:52 -0700

    Add babel

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,9 +1,11 @@
 var config = require('../gulp.config');
 var gulp = require('gulp');
+var babel = require('gulp-babel');
 var webpack = require('gulp-webpack');
 
 gulp.task('build:js', function() {
     return gulp.src( config.js.entry )
         .pipe( webpack( require('../../webpack.config.js') ) )
+        .pipe( babel() )
         .pipe( gulp.dest( config.js.build ));
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,2 @@
-var angular = require('angular');
+var evens = [2, 4, 6, 8];
+var odds = evens.map( v => v + 1);
