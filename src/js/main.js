ab1bafd13dd1d14b456fd106c6c385d4c2272c7d
mitch-rickman <mitchrickman@gmail.com>
2016-05-18 22:02:39 -0700

    Angular setup

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,27 @@
-var evens = [2, 4, 6, 8];
-var odds = evens.map( v => v + 2);
+import angular from 'angular';
+
+var reminderApp = angular.module('reminderApp', []);
+
+reminderApp.controller('reminderList', function() {
+
+    this.reminders = [
+        {
+            name: "Test reminder",
+            timeframe: {
+                increment: "hour",
+                start: "9:00 AM",
+                end: "5:00 PM"
+            },
+
+        },
+        {
+            name: "Go for a jog",
+            timeframe: {
+                increment: "day",
+                start: "9:00 AM",
+                end: "5:00 PM"
+            },
+
+        }
+    ];
+})
